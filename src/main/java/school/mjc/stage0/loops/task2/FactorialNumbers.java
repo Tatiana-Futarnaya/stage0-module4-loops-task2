c70d11a0b37b15990dce64bda3596c3be9fed839
Tatiana-Futarnaya <tannia93@mail.ru>
2023-02-11 14:12:03 +0300

    first commit

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-
+        int i=0;
+        int result=1;
+        while (i<=printToInclusive){
+            if(i==0){
+                result=result*(int)Math.pow(i,0);
+            }else{
+                result=result*i;
+            }
+            System.out.println(result);
+            i++;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int i=2;
+
+        while (i<=printToInclusive){
+            boolean isPrime = true;
+            double squareRoot = Math.sqrt(i) ;
+            int r = 2;
+            while (r <= squareRoot && isPrime) {
+                if (i % r == 0){
+                    isPrime = false;
+                    break;
+                }
+                r++;
+            }
+            System.out.print(isPrime ? (i + "\n") : "");
+            i++;
+        }
     }
 }
